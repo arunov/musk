ebe2d633fe3cd1e4583f08bfb485ca2757848562
Yongming Shen <symingz@gmail.com>
2014-02-16 18:34:06 -0500

    Decoder third initial draft.

--- Decoder.sv
+++ Decoder.sv
@@ -1,11 +1,13 @@
 
-`define MAX_OPNAME_LEN 32
-
-typedef enum {} opcode_mode_t;
+typedef logic[0:32*8-1] opcode_name_t;
+typedef logic[0:9*8-1] opcode_mode_t;
+typedef enum {OP_MPX_NIL, OP_MPX_66H, OP_MPX_F2H, OP_MPX_F3H} opcode_mprefix_t;
 
 typedef struct packed {
-	logic[
-} opcode_t;
+	opcode_name_t name;
+	opcode_mode_t mode;
+	opcode_mprefix_t mprefix;
+} opcode_struct_t;
 
 typedef struct packed {
 	logic[0:7] lock_repeat_prefix;
@@ -13,6 +15,7 @@ typedef struct packed {
 	logic[0:7] operand_size_prefix;
 	logic[0:7] address_size_prefix;
 	logic[0:7] rex_prefix;
+	opcode_struct_t opcode_struct;
 } fat_instruction_t;
 
 function automatic logic is_lock_repeat_prefix(logic[0:7] val);
