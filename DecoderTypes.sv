446a0858ec0ffb384cd04851bddf4a53b65b1e58
Soujanya <soujanya@fsl.cs.sunysb.edu>
2014-03-05 20:36:30 -0500

    operand_buffer introduced to populate info from instruction decoder

--- DecoderTypes.sv
+++ DecoderTypes.sv
@@ -11,6 +11,14 @@ typedef struct packed {
 	logic[4:0] group;
 } opcode_struct_t;
 
+/* Structure that represents an operand */
+typedef struct packed{
+    logic[3:0] reg_id;
+    logic[63:0] immediate;
+    /* Bitmap to indicate which fields in this structure are valid*/
+    logic[1:0] bitmap;
+} decode_buff_t;
+
 typedef struct packed {
 	logic[7:0] lock_repeat_prefix;
 	logic[7:0] segment_branch_prefix;
@@ -19,6 +27,9 @@ typedef struct packed {
 	logic[7:0] rex_prefix;
 	opcode_struct_t opcode_struct;
 	logic operands_use_modrm;
+    /* Placeholder for storing src and destination operands */
+    decode_buff_t opa;
+    decode_buff_t opb;
 } fat_instruction_t;
 
 `endif /* _DECODER_TYPES_ */
