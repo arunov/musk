1b808eb7156f86f764727434409b6ed4b0035404
soujanyas <soujanya@fsl.cs.sunysb.edu>
2014-03-06 20:18:15 -0500

    Converge fail in verilator bug fixed in ALU

--- ALU.sv
+++ ALU.sv
@@ -5,7 +5,7 @@
 
 `define readval(X) \
 	case (fat_inst.op``X``.bitmap) \
-		`REG_BITMAP: val``X = reg_file_out[`get_reg_in_file(fat_inst.op``X``.reg_id)]; \
+		`REG_BITMAP: val``X = `get_64(reg_file_in, fat_inst.op``X``.reg_id); \
 		`IMM_BITMAP: val``X = fat_inst.op``X``.immediate; \
 		default: val``X = fat_inst.op``X``.immediate; \
 	endcase
@@ -15,7 +15,7 @@
 function automatic logic ALU(
 	`LINTOFF_UNUSED(fat_instruction_t fat_inst),
 	logic[0:16*64-1] reg_file_in, 
-	`LINTOFF_UNOPTFLAT(output logic[0:16*64-1] reg_file_out));
+	output logic[0:16*64-1] reg_file_out);
 
 	logic[63:0] vala, valb;
 
@@ -23,16 +23,14 @@ function automatic logic ALU(
 	`readval(b)
 
 	reg_file_out = reg_file_in;
-
 	case (fat_inst.opcode_struct.name)
 		`dobinop(imul, *)
 		`dobinop(add, +)
 		`dobinop(or, |)
-		"mov": reg_file_out[`get_reg_in_file(fat_inst.opa.reg_id)] = valb;
-		"movabs": reg_file_out[`get_reg_in_file(fat_inst.opa.reg_id)] = valb;
-		default: reg_file_out = reg_file_in;
+		`dobinop(and, &)
+		"mov": `get_64(reg_file_out, fat_inst.opa.reg_id) = valb;
+		"movabs": `get_64(reg_file_out, fat_inst.opa.reg_id) = valb;
 	endcase
-
 
 	//$display("%s", fat_inst.opcode_struct.name);
 	return fat_inst.opcode_struct.name == "retq";
