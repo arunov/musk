82f8af9131271e506f5285ba57a5e6476fcd5669
Varun Agrawal <vagrawal@cs.stonybrook.edu>
2014-04-16 21:59:13 -0400

    SRAM interface modified
    Use writeEnable to specify which words in the 64 bytes of writeData
    are to be written to the SRAM. Other words are ignored.

--- SRAM.sv
+++ SRAM.sv
@@ -1,5 +1,16 @@
-module SRAM(input[width-1:0] writeData, output[width-1:0] readData, input[logDepth-1:0] writeAddr, input[logDepth-1:0] readAddr, input writeEnable, input clk);
-	parameter width=16, logDepth=9, ports=1, delay=(logDepth-8>0?logDepth-8:1)*(ports>1?(ports>2?(ports>3?100:20):14):10)/10-1;
+module SRAM(
+    input                   clk
+,   input   [logDepth-1:0]  readAddr
+,   output  [   width-1:0]  readData
+,   input   [logDepth-1:0]  writeAddr
+,   input   [   width-1:0]  writeData
+,   input   [width/wordsize-1:0]   writeEnable
+);
+	parameter   width   =512
+    ,           logDepth=9
+    ,           wordsize=64
+    ,           ports   =1
+    ,           delay   =(logDepth-8>0?logDepth-8:1)*(ports>1?(ports>2?(ports>3?100:20):14):10)/10-1;
 
 	logic[width-1:0] mem[(1<<logDepth)-1:0];
 
@@ -10,16 +21,24 @@ module SRAM(input[width-1:0] writeData, output[width-1:0] readData, input[logDep
 		assert(ports == 1) else $fatal("multi-ported SRAM not supported");
 	end
 
+	integer i;
+
 	always @ (posedge clk) begin
-		if (delay > 0) begin
-			readpipe[0] <= mem[readAddr];
-			for(int i=1; i<delay; ++i) readpipe[i] <= readpipe[i-1];
-			readData <= readpipe[delay-1];
-		end else
-		begin
-			readData <= mem[readAddr];
+
+		if (delay > 0)  begin
+			readpipe[0]     <= mem[readAddr];
+			for(i=1; i<delay; ++i)
+				readpipe[i] <= readpipe[i-1];
+				readData        <= readpipe[delay-1];
+			end
+		else            begin
+			readData        <= mem[readAddr];
 		end
-		if (writeEnable)
-			mem[writeAddr] <= writeData;
+
+		for ( i=0; i<width/wordsize; i=i+1 ) begin
+			if (writeEnable[i]) begin
+				mem[writeAddr][i*wordsize+:wordsize] <= writeData[i*wordsize+:wordsize];
+			end
+		end
 	end
 endmodule
