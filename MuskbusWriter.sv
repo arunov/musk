37ca91abe207aac4a64e88a87bc899ea963c9152
Yongming Shen <symingz@gmail.com>
2014-04-18 18:55:35 -0400

    MuskbusWriter.sv done

--- MuskbusWriter.sv
+++ MuskbusWriter.sv
@@ -11,46 +11,53 @@ module MuskbusWriter (
 	input logic [0:64*8-1] data
 );
 
-	enum { idle, init, sending, serving } state_ff, new_state_cb;
+	enum { idle, writing, serving } state_ff, new_state_cb;
 	int offset_ff;
+	logic got_first_ack_ff;
 
 	always_ff @ (posedge clk) begin
 		if (reset) begin
 			state_ff <= idle;
-			offset_ff <= 0;
 		end else begin
 			state_ff <= new_state_cb;
 		end
 
-		if (new_state_cb == idle) offset_ff <= 0;
+		if (reset) begin
+			offset_ff <= 0;
+			got_first_ack_ff <= 0;
+		end
 
-		if (state_ff == sending && bus_reqack) begin
+		if (got_first_ack_ff && bus_reqack) begin
 			offset_ff <= offset_ff + 64;
+		end
+
+		if (new_state_cb == idle) begin
+			offset_ff <= 0;
+			got_first_ack_ff <= 0;
 		end
 	end
 
 	always_comb begin
 		new_state_cb = state_ff;
 		unique case(state_ff)
-			idle : if (reqcyc) new_state_cb = init;
-			init : if (bus_reqack) new_state_cb = sending;
-			sending : if (offset_ff == 64 * 8) new_state_cb = serving;
-			serving : if (!reqcyc) new_state_cb = idle;
+			idle : if (reqcyc) new_state_cb = writing;
+			writing : if (offset_ff == 64 * 8) new_state_cb = serving;
+			serving : new_state_cb = idle;
 		endcase
 
 		bus_req = 0;
 		bus_respack = 0;
 		respcyc = 0;
 
-		if (new_state_cb == init || new_state_cb == sending) bus_req.bid = 1;
+		if (new_state_cb == writing) bus_req.bid = 1;
 
-		if (new_state_cb == init) begin
+		if (new_state_cb == writing && !got_first_ack_ff) begin
 			bus_req.reqcyc = 1;
 			bus_req.reqtag = MUSKBUS::WRITE_MEM_TAG;
 			bus_req.req = addr;
 		end
 
-		if (state_ff == sending && new_state_cb != serving) begin
+		if (new_state_cb == writing && got_first_ack_ff) begin
 			bus_req.reqcyc = 1;
 			bus_req.reqtag = MUSKBUS::WRITE_MEM_TAG;
 			bus_req.req = data[offset_ff +: 64];
@@ -59,7 +66,6 @@ module MuskbusWriter (
 		if (new_state_cb == serving) begin
 			respcyc = 1;
 		end
-
 	end
 
 endmodule
