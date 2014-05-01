afb3ede4550a3497658cc6db2e231e0238e76173
Arun Olappamanna Vasudevan <arunov1986@gmail.com>
2014-05-01 15:52:27 -0400

    temp

--- InstructionPrinter.sv
+++ InstructionPrinter.sv
@@ -3,4 +3,10 @@ package InstructionPrinter;
 import DecoderTypes::*;
 import RegMap::*;
 
+function prtInstr(fat_instruction_t ins);
+	$WRITE("%s", opcode_struct.name);
+	
+
+endfunction
+
 endpackage
