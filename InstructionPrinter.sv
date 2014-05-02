04e1b1b91261babfbb27b76e2fc8e9be5dc9799f
Arun Olappamanna Vasudevan <arunov1986@gmail.com>
2014-05-02 03:30:37 -0400

    changes to make new output similar to old

--- InstructionPrinter.sv
+++ InstructionPrinter.sv
@@ -9,19 +9,31 @@ function logic prtOpd(/* verilator lint_off UNUSED */ operand_t opd /* verilator
 		return 1;
 	end
 	unique case(opd.opd_type)
-		opdt_register:
-			$write("%s", reg_id2name(opd.base_reg));
+		opdt_register: begin
+			/* verilator lint_off WIDTH */
+			logic[0:4*8-1] regname = reg_id2name(opd.base_reg);
+			/* verilator lint_on WIDTH */
+			$write("%s", regname);
+		end
 		opdt_immediate:
 			$write("0x%x", opd.immediate);
 		opdt_memory: begin
 			if(opd.mem_has_disp)
 				$write("0x%x", opd.immediate);
 			$write("(");
-			if(opd.mem_has_base)
-				$write("%s", opd.base_reg);
+			if(opd.mem_has_base) begin
+				/* verilator lint_off WIDTH */
+				logic[0:4*8-1] regname = reg_id2name(opd.base_reg);
+				/* verilator lint_on WIDTH */
+				$write("%s", regname);
+			end
 			$write(",");
-			if(opd.mem_has_index)
-				$write("%s", opd.index_reg);
+			if(opd.mem_has_index) begin
+				/* verilator lint_off WIDTH */
+				logic[0:4*8-1] regname = reg_id2name(opd.base_reg);
+				/* verilator lint_on WIDTH */
+				$write("%s", regname);
+			end
 			$write(",");
 			//if(opd.mem_has_scale)
 				$write("0x%x", opd.scale);
@@ -34,9 +46,9 @@ function logic prtOpd(/* verilator lint_off UNUSED */ operand_t opd /* verilator
 endfunction
 
 function void prtInstr(/* verilator lint_off UNUSED */ fat_instruction_t ins /* verilator lint_on UNUSED */);
-	$write("%s ", ins.opcode_struct.name);
+	$write("%s  \t", ins.opcode_struct.name);
 	if(prtOpd(ins.operand0) && ins.operand1.opd_type != opdt_nil)
-		$write(",");
+		$write(", ");
 	prtOpd(ins.operand1);
 endfunction
 
