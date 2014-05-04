af431dbcd4decb1e28dfb3f13d42d6f435b6b58a
Arun Olappamanna Vasudevan <arunov1986@gmail.com>
2014-05-04 10:13:24 -0400

    script that runs and diffs with old output

--- runall.sh
+++ runall.sh
@@ -3,3 +3,44 @@ make prog n=2 > prog2out
 make prog n=3 > prog3out
 make prog n=4 > prog4out
 make prog n=5 > prog5out
+
+if [ -e ../prog1out ]
+then
+	echo prog1 diff > diffout
+	echo ========== >> diffout
+	diff ../prog1out prog1out >> diffout
+	echo >> diffout
+fi
+
+if [ -e ../prog2out ]
+then
+	echo prog2 diff >> diffout
+	echo ========== >> diffout
+	diff ../prog2out prog2out >> diffout
+	echo >> diffout
+fi
+
+if [ -e ../prog3out ]
+then
+	echo prog3 diff >> diffout
+	echo ========== >> diffout
+	diff ../prog3out prog3out >> diffout
+	echo >> diffout
+fi
+
+if [ -e ../prog4out ]
+then
+	echo prog4 diff >> diffout
+	echo ========== >> diffout
+	diff ../prog4out prog4out >> diffout
+	echo >> diffout
+fi
+
+if [ -e ../prog5out ]
+then
+	echo prog5 diff >> diffout
+	echo ========== >> diffout
+	diff ../prog5out prog5out >> diffout
+	echo >> diffout
+fi
+
